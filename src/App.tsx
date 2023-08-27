f951d04976b6c17fa3b01b735f8cbf15f6c80fba
Harkeerat Kanwal <hsk532991@gmail.com>
2023-08-27 19:45:42 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+import { Canvas } from '@react-three/fiber'
+
+function App() {
+  return (
+    <div id='canvas-container'>
+
+    <Canvas>
+      <ambientLight intensity={0.1} />
+      <directionalLight color="white" position={[0, 0, 5]} />
+
+      <mesh position={[0, 0, 0]}>
+        <sphereGeometry args={[1, 64, 32]} />
+        <meshStandardMaterial color="red" />
+      </mesh>
+    </Canvas>
+    </div>
+  )
+}
+
+export default App
