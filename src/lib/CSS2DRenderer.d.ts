b7dd8ff1d106cdc80d804da9735b3df6c6611725
Harkeerat Kanwal <hsk532991@gmail.com>
2023-08-27 22:16:50 -0400

    Add constructor type definitions

--- src/lib/CSS2DRenderer.d.ts
+++ src/lib/CSS2DRenderer.d.ts
@@ -1,12 +1,14 @@
 import { Vector2, Object3D, Scene, Camera } from "@types/three";
 
 export declare class CSS2DObject extends Object3D {
+  constructor(element?: HTMLDivElement);
   isCSS2DObject: boolean;
   element: HTMLDivElement;
   center: Vector2;
 }
 
 export declare class CSS2DRenderer {
+  constructor(parameters?: { element?: HTMLDivElement });
   domElement: HTMLDivElement;
   getSize: () => { width: number; height: number };
   setSize: (width: number, height: number) => void;
