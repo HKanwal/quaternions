0ed7e6aa7530fedb9ed30924b857dbd929bbb8e8
Harkeerat Kanwal <hsk532991@gmail.com>
2023-08-28 03:32:56 -0400

    Change FormControls to TextFields

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,45 +1,45 @@
-import { FormControl, InputAdornment, OutlinedInput } from "@mui/material";
+import { FormControl, InputAdornment, OutlinedInput, TextField } from "@mui/material";
 import Styles from "../styles/Controls.module.css";
 import AddIcon from "@mui/icons-material/Add";
 
 function Controls() {
   return (
     <div className={Styles.container}>
-      <FormControl sx={{ m: 1, width: "8ch", display: "inline-block" }} variant="outlined">
-        <OutlinedInput
-          id="outlined-adornment-weight"
-          endAdornment={
+      <TextField
+        sx={{ m: 1, width: "8ch" }}
+        InputProps={{
+          endAdornment: (
             <InputAdornment position="end">
               <i>i</i>
             </InputAdornment>
-          }
-          size="small"
-        />
-      </FormControl>
+          ),
+        }}
+        size="small"
+      />
       <AddIcon />
-      <FormControl sx={{ m: 1, width: "8ch", display: "inline-block" }} variant="outlined">
-        <OutlinedInput
-          id="outlined-adornment-weight"
-          endAdornment={
+      <TextField
+        sx={{ m: 1, width: "8ch" }}
+        InputProps={{
+          endAdornment: (
             <InputAdornment position="end">
               <i>j</i>
             </InputAdornment>
-          }
-          size="small"
-        />
-      </FormControl>
+          ),
+        }}
+        size="small"
+      />
       <AddIcon />
-      <FormControl sx={{ m: 1, width: "8ch", display: "inline-block" }} variant="outlined">
-        <OutlinedInput
-          id="outlined-adornment-weight"
-          endAdornment={
+      <TextField
+        sx={{ m: 1, width: "8ch" }}
+        InputProps={{
+          endAdornment: (
             <InputAdornment position="end">
               <i>k</i>
             </InputAdornment>
-          }
-          size="small"
-        />
-      </FormControl>
+          ),
+        }}
+        size="small"
+      />
     </div>
   );
 }
