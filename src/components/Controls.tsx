5efca93b4027c80cf90e6f59de7a7d05ca732c33
Harkeerat Kanwal <hsk532991@gmail.com>
2023-08-28 04:57:18 -0400

    Format numbers on blur

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -7,17 +7,46 @@ const numericDigits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
 
 function containsNonNumeric(str: string) {
   let invalid = false;
+  let numPeriods = 0;
   const splitStr = str.split("");
 
   for (const char of splitStr) {
     if (!numericDigits.includes(char)) {
       console.log(char);
       invalid = true;
+    } else if (char === ".") {
+      numPeriods++;
     }
   }
 
-  return invalid;
+  return numPeriods > 1 || invalid;
 }
+
+function formatNumber(str: string) {
+  if (str.length < 4) {
+    if (str.includes(".")) {
+      let [pre, post] = str.split(".");
+
+      if (pre.length < 1) {
+        pre = "0";
+      }
+
+      if (post.length === 0) {
+        post = "00";
+      } else if (post.length === 1) {
+        post += "0";
+      }
+
+      return `${pre}.${post}`;
+    } else {
+      return `${str}.00`;
+    }
+  } else {
+    return str;
+  }
+}
+
+type BasisVector = "i" | "j" | "k";
 
 function Controls() {
   const [axis, setAxis] = useState<[string, string, string]>(["1.00", "0.00", "0.00"]);
@@ -25,7 +54,7 @@ function Controls() {
   const jRef = useRef<HTMLInputElement>(null);
   const kRef = useRef<HTMLInputElement>(null);
 
-  const handleChange = (basisVector: "i" | "j" | "k", newVal: string) => {
+  const handleChange = (basisVector: BasisVector, newVal: string) => {
     if (newVal.length > 4 || parseFloat(newVal) > 1 || containsNonNumeric(newVal)) {
       return;
     }
@@ -43,7 +72,7 @@ function Controls() {
     }
   };
 
-  const handleClick = (basisVector: "i" | "j" | "k") => {
+  const handleClick = (basisVector: BasisVector) => {
     switch (basisVector) {
       case "i":
         iRef.current?.focus();
@@ -56,6 +85,27 @@ function Controls() {
       case "k":
         kRef.current?.focus();
         kRef.current?.setSelectionRange(0, kRef.current?.value.length);
+        break;
+    }
+  };
+
+  const handleBlur = (basisVector: BasisVector) => {
+    const ref = basisVector === "i" ? iRef : basisVector === "j" ? jRef : kRef;
+    const val = ref.current?.value;
+
+    if (val === undefined) {
+      return;
+    }
+
+    switch (basisVector) {
+      case "i":
+        setAxis([formatNumber(val), axis[1], axis[2]]);
+        break;
+      case "j":
+        setAxis([axis[0], formatNumber(val), axis[2]]);
+        break;
+      case "k":
+        setAxis([axis[0], axis[1], formatNumber(val)]);
         break;
     }
   };
@@ -81,6 +131,7 @@ function Controls() {
         value={axis[0]}
         onChange={(e) => handleChange("i", e.target.value)}
         onClick={() => handleClick("i")}
+        onBlur={() => handleBlur("i")}
       />
       <AddIcon />
       <TextField
@@ -98,6 +149,7 @@ function Controls() {
         value={axis[1]}
         onChange={(e) => handleChange("j", e.target.value)}
         onClick={() => handleClick("j")}
+        onBlur={() => handleBlur("j")}
       />
       <AddIcon />
       <TextField
@@ -115,6 +167,7 @@ function Controls() {
         value={axis[2]}
         onChange={(e) => handleChange("k", e.target.value)}
         onClick={() => handleClick("k")}
+        onBlur={() => handleBlur("k")}
       />
     </div>
   );
