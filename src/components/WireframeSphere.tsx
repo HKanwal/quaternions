88a1c1eed07d3d7c57d7cf314fce2cfda3a3e9b8
Harkeerat Kanwal <hsk532991@gmail.com>
2023-08-27 20:20:40 -0400

    Decrease width and heigh segments

--- src/components/WireframeSphere.tsx
+++ src/components/WireframeSphere.tsx
@@ -1,5 +1,8 @@
 import { useThree } from "@react-three/fiber";
 
+const widthSegments = 24;
+const heightSegments = 12;
+
 function WireframeSphere() {
   useThree(({ camera }) => {
     camera.position.set(5, 5, 5);
@@ -8,12 +11,12 @@ function WireframeSphere() {
   return (
     <>
       <mesh position={[0, 0, 0]}>
-        <sphereGeometry args={[1, 64, 32]} />
+        <sphereGeometry args={[1, widthSegments, heightSegments]} />
         <meshBasicMaterial color="black" wireframe />
       </mesh>
 
       <mesh position={[0, 0, 0]}>
-        <sphereGeometry args={[1, 64, 32]} />
+        <sphereGeometry args={[1, widthSegments, heightSegments]} />
         <meshBasicMaterial color="grey" />
       </mesh>
     </>
