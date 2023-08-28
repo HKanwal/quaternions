b1e0be1a9b95534ff9d3a2a5e7fe3e668e8fb438
Harkeerat Kanwal <hsk532991@gmail.com>
2023-08-27 20:34:02 -0400

    Add placeholder overlay in bot-right.

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,22 @@ import { OrbitControls } from "@react-three/drei";
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import WireframeSphere from "./components/WireframeSphere";
+import Controls from "./components/Controls";
 
 function App() {
   return (
-    <div id="canvas-container">
-      <Canvas>
-        <WireframeSphere />
+    <>
+      <div id="canvas-container">
+        <Canvas>
+          <WireframeSphere />
 
-        <gridHelper args={[20, 20]} />
-        <OrbitControls />
-      </Canvas>
-    </div>
+          <gridHelper args={[20, 20]} />
+          <OrbitControls />
+        </Canvas>
+      </div>
+
+      <Controls />
+    </>
   );
 }
 
---
+++ src/components/Controls.tsx
@@ -0,0 +1,11 @@
+import Styles from "../styles/Controls.module.css";
+
+function Controls() {
+  return (
+    <div className={Styles.container}>
+      <span>Hello!</span>
+    </div>
+  );
+}
+
+export default Controls;
