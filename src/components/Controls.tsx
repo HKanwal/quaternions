8cfd51784b2ba056dff9011ece8f8f87a5b2f263
Harkeerat Kanwal <hsk532991@gmail.com>
2023-08-28 03:22:01 -0400

    Start adding inputs

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,9 +1,44 @@
+import { FormControl, InputAdornment, OutlinedInput } from "@mui/material";
 import Styles from "../styles/Controls.module.css";
 
 function Controls() {
   return (
     <div className={Styles.container}>
-      <span>Hello!</span>
+      <FormControl sx={{ m: 1, width: "8ch", display: "inline-block" }} variant="outlined">
+        <OutlinedInput
+          id="outlined-adornment-weight"
+          endAdornment={
+            <InputAdornment position="end">
+              <i>i</i>
+            </InputAdornment>
+          }
+          size="small"
+        />
+      </FormControl>
+      +
+      <FormControl sx={{ m: 1, width: "8ch", display: "inline-block" }} variant="outlined">
+        <OutlinedInput
+          id="outlined-adornment-weight"
+          endAdornment={
+            <InputAdornment position="end">
+              <i>j</i>
+            </InputAdornment>
+          }
+          size="small"
+        />
+      </FormControl>
+      +
+      <FormControl sx={{ m: 1, width: "8ch", display: "inline-block" }} variant="outlined">
+        <OutlinedInput
+          id="outlined-adornment-weight"
+          endAdornment={
+            <InputAdornment position="end">
+              <i>k</i>
+            </InputAdornment>
+          }
+          size="small"
+        />
+      </FormControl>
     </div>
   );
 }
