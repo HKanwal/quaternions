1458a169a7a76b7c8267dcec6dcfbbc51455a291
Harkeerat Kanwal <hsk532991@gmail.com>
2023-08-28 00:49:21 -0400

    Use capital snake case for constants

--- src/components/WireframeSphere.tsx
+++ src/components/WireframeSphere.tsx
@@ -1,7 +1,7 @@
 import { useThree } from "@react-three/fiber";
 
-const widthSegments = 24;
-const heightSegments = 12;
+const WIDTH_SEGMENT = 24;
+const HEIGHT_SEGMENTS = 12;
 
 function WireframeSphere() {
   useThree(({ camera }) => {
@@ -11,12 +11,12 @@ function WireframeSphere() {
   return (
     <>
       <mesh position={[0, 0, 0]}>
-        <sphereGeometry args={[1, widthSegments, heightSegments]} />
+        <sphereGeometry args={[1, WIDTH_SEGMENT, HEIGHT_SEGMENTS]} />
         <meshBasicMaterial color="black" wireframe />
       </mesh>
 
       <mesh position={[0, 0, 0]}>
-        <sphereGeometry args={[1, widthSegments, heightSegments]} />
+        <sphereGeometry args={[1, WIDTH_SEGMENT, HEIGHT_SEGMENTS]} />
         <meshBasicMaterial color="grey" />
       </mesh>
     </>
