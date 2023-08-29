5464dbf010674faa0371bde1226d6e13a51e8eab
Harkeerat Kanwal <hsk532991@gmail.com>
2023-08-29 00:30:52 -0400

    Add a point at i

--- src/components/WireframeSphere.tsx
+++ src/components/WireframeSphere.tsx
@@ -4,32 +4,40 @@ import * as THREE from "three";
 const WIDTH_SEGMENTS = 24;
 const HEIGHT_SEGMENTS = 12;
 
+const POINT_WIDTH_SEGMENTS = 32;
+const POINT_HEIGHT_SEGMENTS = 16;
+
 interface WireframeSphereProps {
   quaternion?: THREE.Quaternion;
 }
 
 function WireframeSphere({ quaternion }: WireframeSphereProps) {
-  const sphereRef = useRef<THREE.Mesh>(null);
-  const wireframeRef = useRef<THREE.Mesh>(null);
+  const groupRef = useRef<THREE.Group>(null);
 
   useEffect(() => {
-    if (quaternion) {
-      sphereRef.current?.setRotationFromQuaternion(quaternion);
-      wireframeRef.current?.setRotationFromQuaternion(quaternion);
+    if (quaternion && groupRef.current) {
+      groupRef.current.setRotationFromQuaternion(quaternion);
     }
   }, [quaternion]);
 
   return (
     <>
-      <mesh position={[0, 0, 0]} ref={wireframeRef}>
-        <sphereGeometry args={[1, WIDTH_SEGMENTS, HEIGHT_SEGMENTS]} />
-        <meshBasicMaterial color="black" wireframe />
-      </mesh>
+      <group ref={groupRef}>
+        <mesh position={[0, 0, 0]}>
+          <sphereGeometry args={[1, WIDTH_SEGMENTS, HEIGHT_SEGMENTS]} />
+          <meshBasicMaterial color="black" wireframe />
+        </mesh>
 
-      <mesh position={[0, 0, 0]} ref={sphereRef}>
-        <sphereGeometry args={[1, WIDTH_SEGMENTS, HEIGHT_SEGMENTS]} />
-        <meshBasicMaterial color="grey" />
-      </mesh>
+        <mesh position={[0, 0, 0]}>
+          <sphereGeometry args={[1, WIDTH_SEGMENTS, HEIGHT_SEGMENTS]} />
+          <meshBasicMaterial color="grey" />
+        </mesh>
+
+        <mesh position={[1, 0, 0]}>
+          <sphereGeometry args={[0.1, POINT_WIDTH_SEGMENTS, POINT_HEIGHT_SEGMENTS]} />
+          <meshBasicMaterial color="#00FF00" />
+        </mesh>
+      </group>
     </>
   );
 }
