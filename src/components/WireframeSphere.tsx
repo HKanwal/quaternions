b7263710cfb7defc6dd4577ce05cd3167fdeb5ca
Harkeerat Kanwal <hsk532991@gmail.com>
2023-08-30 01:23:53 -0400

    Add labels for points

--- src/components/WireframeSphere.tsx
+++ src/components/WireframeSphere.tsx
@@ -1,6 +1,7 @@
 import { useFrame } from "@react-three/fiber";
-import { useMemo, useRef } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import * as THREE from "three";
+import { CSS2DObject } from "../lib/CSS2DRenderer";
 
 const WIDTH_SEGMENTS = 24;
 const HEIGHT_SEGMENTS = 12;
@@ -14,6 +15,9 @@ interface WireframeSphereProps {
 
 function WireframeSphere({ quaternion }: WireframeSphereProps) {
   const groupRef = useRef<THREE.Group>(null);
+  const iRef = useRef<THREE.Mesh>(null);
+  const jRef = useRef<THREE.Mesh>(null);
+  const kRef = useRef<THREE.Mesh>(null);
   const dynamicQuaternion = useMemo(() => {
     return new THREE.Quaternion(0, 0, 0, 1);
   }, []);
@@ -26,6 +30,32 @@ function WireframeSphere({ quaternion }: WireframeSphereProps) {
     dynamicQuaternion.slerp(quaternion, 0.05);
     groupRef.current.setRotationFromQuaternion(dynamicQuaternion);
   });
+
+  useEffect(() => {
+    const iDiv = document.createElement("div");
+    iDiv.className = "point-label";
+    iDiv.textContent = "i";
+
+    const iLabel = new CSS2DObject(iDiv);
+    iLabel.position.set(0.15, 0, 0);
+    iRef.current?.add(iLabel);
+
+    const jDiv = document.createElement("div");
+    jDiv.className = "point-label";
+    jDiv.textContent = "j";
+
+    const jLabel = new CSS2DObject(jDiv);
+    jLabel.position.set(0, 0, -0.15);
+    jRef.current?.add(jLabel);
+
+    const kDiv = document.createElement("div");
+    kDiv.className = "point-label";
+    kDiv.textContent = "k";
+
+    const kLabel = new CSS2DObject(kDiv);
+    kLabel.position.set(0, 0.15, 0);
+    kRef.current?.add(kLabel);
+  }, []);
 
   return (
     <>
@@ -40,17 +70,17 @@ function WireframeSphere({ quaternion }: WireframeSphereProps) {
           <meshBasicMaterial color="grey" opacity={0.5} transparent />
         </mesh>
 
-        <mesh position={[1, 0, 0]}>
+        <mesh position={[1, 0, 0]} ref={iRef}>
           <sphereGeometry args={[0.1, POINT_WIDTH_SEGMENTS, POINT_HEIGHT_SEGMENTS]} />
           <meshBasicMaterial color="#FF0000" />
         </mesh>
 
-        <mesh position={[0, 0, -1]}>
+        <mesh position={[0, 0, -1]} ref={jRef}>
           <sphereGeometry args={[0.1, POINT_WIDTH_SEGMENTS, POINT_HEIGHT_SEGMENTS]} />
           <meshBasicMaterial color="#00FF00" />
         </mesh>
 
-        <mesh position={[0, 1, 0]}>
+        <mesh position={[0, 1, 0]} ref={kRef}>
           <sphereGeometry args={[0.1, POINT_WIDTH_SEGMENTS, POINT_HEIGHT_SEGMENTS]} />
           <meshBasicMaterial color="#489fe5" />
         </mesh>
