f725d03eddfc317f3ff865afce81bb12f6765189
Harkeerat Kanwal <hsk532991@gmail.com>
2023-09-01 18:32:16 -0400

    Responsive quaternion display font sizes

--- src/components/Quaternion.tsx
+++ src/components/Quaternion.tsx
@@ -1,4 +1,6 @@
 import Var from "./Var";
+import Styles from "../styles/Quaternions.module.css";
+import { useEffect, useState } from "react";
 
 function Plus() {
   return <span style={{ margin: "0 3px 0 3px", display: "block" }}>+</span>;
@@ -10,23 +12,29 @@ interface QuaternionProps {
 }
 
 function Quaternion(props: QuaternionProps) {
+  const [width, setWidth] = useState(() => window.innerWidth);
+
+  useEffect(() => {
+    function onResize() {
+      setWidth(window.innerWidth);
+    }
+
+    window.addEventListener("resize", onResize);
+
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
+
   return (
-    <div
-      style={{
-        width: "100%",
-        fontSize: "18px",
-        display: "flex",
-        justifyContent: "center",
-        flexWrap: "wrap",
-      }}
-    >
+    <div className={Styles.container}>
       <span>cos({props.angle}&deg;/2)</span>
       <Plus />
       <span style={{ display: "flex" }}>
         sin({props.angle}&deg;/2) ({props.axis[0]}
-        <Var letter="i" fontSize="20px" /> <Plus /> {props.axis[1]}
-        <Var letter="j" fontSize="20px" /> <Plus /> {props.axis[2]}
-        <Var letter="k" fontSize="20px" />)
+        <Var letter="i" fontSize={width < 500 ? "4vmin" : "20px"} /> <Plus /> {props.axis[1]}
+        <Var letter="j" fontSize={width < 500 ? "4vmin" : "20px"} /> <Plus /> {props.axis[2]}
+        <Var letter="k" fontSize={width < 500 ? "4vmin" : "20px"} />)
       </span>
     </div>
   );
