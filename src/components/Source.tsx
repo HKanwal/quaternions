c8a071c656e177363ba9d87b6509560edff4cb6e
Harkeerat Kanwal <hsk532991@gmail.com>
2023-09-02 01:20:03 -0400

    Change source color

--- src/components/Source.tsx
+++ src/components/Source.tsx
@@ -4,7 +4,7 @@ import { Fab } from "@mui/material";
 function Source() {
   return (
     <div style={{ position: "absolute", right: "20px", top: "20px", zIndex: 99 }}>
-      <Fab color="error" size="small" href="https://github.com/HKanwal/quaternions">
+      <Fab color="default" size="small" href="https://github.com/HKanwal/quaternions">
         <CodeIcon fontSize="small" />
       </Fab>
     </div>
